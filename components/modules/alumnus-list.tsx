47ba0423471aff9852672f7d4db17a7f1bb66079
kixixixixi <kixixixixi@gmail.com>
2025-03-04 00:16:03 +0900

    Add name filter

--- components/modules/alumnus-list.tsx
+++ components/modules/alumnus-list.tsx
@@ -10,10 +10,12 @@ const AlumnusListComponent: FC<ComponentProps<"div">> = ({
   const [alumni, setAlumni] = useAtom(alumniAtom)
   const [selectedAlumni, setSelectedAlumni] = useAtom(selectedAlumniAtom)
   const [alumnusFilter, setAlumnusFilter] = useState<{
+    name: string
     countryNames: string[]
     completionYears: number[]
     hasPosition: boolean
   }>({
+    name: "",
     countryNames: [],
     completionYears: [2023, 2022, 2021, 2020, 2019, 2018],
     hasPosition: true,
@@ -34,6 +36,13 @@ const AlumnusListComponent: FC<ComponentProps<"div">> = ({
         .filter((alumnus) =>
           alumnusFilter.completionYears.includes(alumnus.completionYear)
         )
+        .filter(
+          (alumnus) =>
+            alumnusFilter.name.length == 0 ||
+            alumnus.name
+              .toLocaleLowerCase()
+              .includes(alumnusFilter.name.toLocaleLowerCase())
+        )
     )
   }, [alumnusFilter])
   return (
@@ -47,38 +56,55 @@ const AlumnusListComponent: FC<ComponentProps<"div">> = ({
         ...style,
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: ".5rem",
-          padding: ".5rem",
-        }}
-      >
-        {[2023, 2022, 2021, 2020, 2019, 2018].map((year) => (
-          <div
-            key={year}
-            style={{
-              border: "1px solid",
-              borderColor: alumnusFilter.completionYears.includes(year)
-                ? "#666"
-                : "#ccc",
-              borderRadius: "1rem",
-              fontSize: ".75rem",
-              padding: ".15rem .5rem",
-            }}
-            onClick={() =>
-              setAlumnusFilter({
-                ...alumnusFilter,
-                completionYears: alumnusFilter.completionYears.includes(year)
-                  ? alumnusFilter.completionYears.filter((y) => y !== year)
-                  : [...alumnusFilter.completionYears, year],
-              })
-            }
-          >
-            {year}
-          </div>
-        ))}
+      <div style={{ padding: ".5rem" }}>
+        <div style={{ fontSize: ".75rem" }}>Completion year</div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: ".5rem",
+            padding: ".25rem 0",
+          }}
+        >
+          {[2023, 2022, 2021, 2020, 2019, 2018].map((year) => (
+            <div
+              key={year}
+              style={{
+                border: "1px solid",
+                borderColor: alumnusFilter.completionYears.includes(year)
+                  ? "#666"
+                  : "#ccc",
+                borderRadius: "1rem",
+                fontSize: ".75rem",
+                padding: ".15rem .5rem",
+              }}
+              onClick={() =>
+                setAlumnusFilter({
+                  ...alumnusFilter,
+                  completionYears: alumnusFilter.completionYears.includes(year)
+                    ? alumnusFilter.completionYears.filter((y) => y !== year)
+                    : [...alumnusFilter.completionYears, year],
+                })
+              }
+            >
+              {year}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div style={{ padding: ".5rem" }}>
+        <div style={{ fontSize: ".75rem" }}>Name</div>
+        <input
+          style={{
+            border: "1px solid #ccc",
+            borderRadius: "1rem",
+            padding: ".25rem .75rem",
+          }}
+          value={alumnusFilter.name}
+          onChange={({ target: { value } }) =>
+            setAlumnusFilter({ ...alumnusFilter, name: value })
+          }
+        />
       </div>
       {alumni.map((alumnus, i) => (
         <div
@@ -103,9 +129,12 @@ const AlumnusListComponent: FC<ComponentProps<"div">> = ({
             &nbsp;
             {alumnus.name}
           </label>
-          <p style={{ fontSize: ".75rem" }}>
+          <p style={{ color: "#666", fontSize: ".75rem" }}>
             {alumnus.countryName}&nbsp;{alumnus.completionYear}&nbsp;
             {alumnus.course}
+          </p>
+          <p style={{ color: "#999", fontSize: ".6rem" }}>
+            {alumnus.affiliationName}
           </p>
         </div>
       ))}
